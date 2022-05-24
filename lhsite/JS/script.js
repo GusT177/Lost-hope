f395a906b75b8575e1de7b586fe17966208f09b1
StazNee <staznee2004@gmail.com>
2022-05-23 22:33:53 -0300

    1.2

---
+++ lhsite/JS/script.js
@@ -0,0 +1,26 @@
+//Sobre
+var sobre = document.querySelector('.header-right .sobre');
+    sobre.addEventListener('mouseenter', ()=>{
+        sobre.style.color = 'red';
+    });
+    sobre.addEventListener('mouseleave', ()=>{
+        sobre.style.color = 'white';
+    });
+
+//Comunnity
+var comunnity = document.querySelector('.header-right .comunnity');
+    comunnity.addEventListener('mouseenter', ()=>{
+        comunnity.style.color = 'red';
+    });
+    comunnity.addEventListener('mouseleave', ()=>{
+        comunnity.style.color = 'white';
+    });
+
+//Vip
+var vip = document.querySelector('.header-right .vips');
+    vip.addEventListener('mouseenter', ()=>{
+        vip.style.color = 'red';
+    });
+    vip.addEventListener('mouseleave', ()=>{
+        vip.style.color = 'white';
+    });
